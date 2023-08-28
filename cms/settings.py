4cc15970e0d22e524c286f9e509d4e291ea82a85
casper moyo <cassymyo@gmail.com>
2023-08-28 10:23:17 +0200

    'production'

--- cms/settings.py
+++ cms/settings.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -8,14 +9,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ju-v3fa#_0&(bf=1zle)m12)y^*e(^&uoq4h9qp@1)w80nu=y#'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
+CSRF_TRUSTED_ORIGINS = ['https://*.up.railway.app/','https://*.127.0.0.1']
 
 # Application definition
 
@@ -71,14 +71,6 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-    # 'default': {
-    #     'ENGINE'  : 'django.db.backends.mysql', # <-- UPDATED line 
-    #     'NAME'    : 'cmsDb',                 # <-- UPDATED line 
-    #     'USER'    : 'root',                     # <-- UPDATED line
-    #     'PASSWORD': 'neverfail',              # <-- UPDATED line
-    #     'HOST'    : 'localhost',                # <-- UPDATED line
-    #     'PORT'    : '3306',
-    # }
 }
 
 
@@ -121,6 +113,7 @@ MEDIA_URL = '/media/'
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = os.path.join(BASE_DIR / 'static/media/')
 
 # Default primary key field type
--- cms/urls.py
+++ cms/urls.py
@@ -6,7 +6,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('base.urls')),
-    path('cms_apis/', include('cms_apis.urls')),
+    # path('cms_apis/', include('cms_apis.urls')),
     path('accountActivity/', include('notis.urls')),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
